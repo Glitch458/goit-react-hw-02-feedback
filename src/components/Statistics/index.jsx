a7814d6cbb19c7eb4b25501544dd55407265283f
Andrey Katrych <andrew.katrich@gmail.com>
2022-07-14 13:09:54 +0300

    fix

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -1,14 +1,13 @@
-import style from './Statistics.module.css'
-import PropTypes from 'prop-types'
+import style from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
-
-const Statistics = ({good, neutral, bad, total, positivePercentage}) => (
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
   <div className={style.div}>
     <p>Good: {good}</p>
     <p>Neutral: {neutral}</p>
     <p>Bad: {bad}</p>
     <p>Total: {total}</p>
-    <p>Positive feedback: {positivePercentage}</p>
+    <p>Positive feedback: {positivePercentage}%</p>
   </div>
 );
 
@@ -17,7 +16,7 @@ Statistics.propTypes = {
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.number.isRequired
-}
+  positivePercentage: PropTypes.number.isRequired,
+};
 
-export default Statistics
+export default Statistics;
